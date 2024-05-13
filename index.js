40c599058c2ab6d0eeb29824d536021d813aeefb
Jose Manuel Cisneros <119477560+Lstrappare@users.noreply.github.com>
2024-05-13 04:54:48 +0000

    Terminando Objetos: BlackMoon y Sucursal

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ const horaActual = new Date().getHours();
 const minutosActuales = new Date().getMinutes();
 
 const BlackMoon = {
-  saldoTotal: 0,
+  saldoTotal: 100,
+  // Arreglo para las sucursales
   sucursales: [],
   // Muestra los datos del Objeto principal
-  mostrarBlackMoon() {
-    return `Saldo total: ${this.saldoTotal} Sucursales: ${this.sucursales}`;
+  mostrarSucursales() {
+    return this.sucursales;
+  },
+  mostrarSaldo() {
+    return `El saldo total es de: ${this.saldoTotal}`;
+  },
+  abonarSaldoSucursal(sucursalID, cantidad){
+    const sucursal = this.sucursales.find(sucursal => sucursal.idSucursal === sucursalID)
+    if(sucursal){
+      if(this.saldoTotal > 0){
+        sucursal.saldoSucursal += cantidad
+        this.saldoTotal -= cantidad
+        return `Se han abonado ${cantidad} a la sucursal ${sucursal.nombreSucursal} con exito`
+      } else {
+        return `No se puede abonar saldo a la sucursal ${sucursal.nombreSucursal} porque el saldo total es 0`
+      }
+    } else {
+      return `No se encontro la sucursal ${sucursalID}`
+    }
   }
+  
 }
 
 class Sucursal {
@@ -16,6 +35,7 @@ class Sucursal {
              codigoPostal, delegacion, horaApertura, minutoApertura, horaCierre, minutoCierre) {
     this.nombreSucursal = nombreSucursal;
     this.idSucursal = idSucursal;
+    this.abierto = false;
     this.saldoSucursal = saldoSucursal;
     this.estado = estado;
     this.calle = calle;
@@ -30,20 +50,20 @@ class Sucursal {
   }
 
   aperturaOCierreAutomatico() {
-    let abierto = false; 
+    this.abierto; 
     if ((horaActual > this.horaApertura || (horaActual === this.horaApertura && 
                                            minutosActuales >= this.minutoApertura)) &&
        (horaActual < this.horaCierre || (horaActual === this.horaCierre && 
                                          minutosActuales < this.minutoCierre))) {
       console.log(`La sucursal ${this.nombreSucursal} está abierta 
       en el estado de ${this.estado}. Son las ${horaActual} y ${minutosActuales}`);
-      abierto = true;
+      this.abierto = true;
     } else {
       console.log(`La sucursal ${this.nombreSucursal} está cerrada en el estado 
       de ${this.estado} por ser las ${horaActual} y ${minutosActuales}`);
-      abierto = false;
+      this.abierto = false;
     }
-    return abierto;
+    return this.abierto;
   }
   
   /*
@@ -78,17 +98,50 @@ class Sucursal {
       this.abierto = true;
     }
   }
+
+  cambiarHoraAperturaYCierre(horaApertura, minutoApertura, horaCierre, minutoCierre){
+    this.horaApertura = horaApertura;
+    this.minutoApertura = minutoApertura;
+    this.horaCierre = horaCierre;
+    this.minutoCierre = minutoCierre;
+  }
+
+  mostrarSaldoSucursal(){
+    return this.saldoSucursal;
+  }
   
-  abonarASaldoTotal() {
-    /*ToDo: Agregar un método que permita abonar el saldo total de la sucursal al saldo 
-    total de BlackMoon. */
+  abonarASaldoTotal(cantidad) {
+    if(this.saldoSucursal > 0 && cantidad <= this.saldoSucursal){
+      BlackMoon.saldoTotal += cantidad;
+      this.saldoSucursal -= cantidad;
+      console.log(`Se ha abonado ${cantidad} al saldo total de BlackMoon con éxito`);
+    } else {
+      console.log(`No se ha podido abonar la cantidad ${cantidad} al saldo total de 
+      BlackMoon ${this.saldoSucursal > 0 ? 'porque no cuentas con saldo suficiente' : 
+                  'porque el saldo de la sucursal es de 0'}`);
+    }
   }
 }
 
 const sucursal1 = new Sucursal('Sucursal Iztacalco', 20213, 0, 'CDMX', 'Coruña', 209, 
-                               9004, 'Iztacalco', 0, 0, 0, 55);
+                               9004, 'Iztacalco', 7, 0, 19, 30);
+
 
 BlackMoon.sucursales.push(sucursal1);
 
-//console.log(sucursal1.aperturaOCierreAutomatico());
-console.log(sucursal1.mostrarInfoSucursal());
+/*console.log(BlackMoon.abonarSaldoSucursal(20213, 50));
+console.log(BlackMoon.abonarSaldoSucursal(20213, 50));*/
+
+class empleados {
+  constructor(nombre, apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+  }
+  
+}
+
+const empleado1 = new empleados('Juan', 'Perez');
+sucursal1.empleados.push(empleado1);
+console.log(sucursal1.abonarASaldoTotal(150));
+console.log(BlackMoon.mostrarSucursales());
+console.log(BlackMoon.mostrarSaldo())
